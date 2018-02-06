ad37f545beb04edd0b34b8ccbae29e40a91d165b
Victor Okon <victor.s.okon@gmail.com>
2018-02-06 16:41:00 -0500

    update for roompicker

--- catalog/react/picker/roompicker/RoomPicker.jsx
+++ catalog/react/picker/roompicker/RoomPicker.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const RoomPicker = ({}) => (
     <div className="roomSelectToggleInput">
         <div className="fieldlabel">Room</div>
+        <div className="toggle">
         <span>Pick A Room</span>
         <img src='../../../images/input-arrow-right.svg' alt="Select a room" />
+        </div>
     </div>
 )
 

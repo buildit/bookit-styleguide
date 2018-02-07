cff78adc454b60cfc33cd7fc421593d8a2b62f72
Victor Okon <victor.s.okon@gmail.com>
2018-02-07 14:48:44 -0500

    adding imports from dates-times and arrows

--- catalog/react/picker/weekspinner/WeekSpinner.jsx
+++ catalog/react/picker/weekspinner/WeekSpinner.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const WeekSpinner= ({}) => (
-<div></div>
-);
+import { getPreviousAndNextWeekDates, formatWeek } from '../../../utils/dates-times.js'
 
-export default WeekSpinner;
+const WeekSpinner = ({ weekOf, onClick }) => {
+    const [previousWeek, nextWeek] = getPreviousAndNextWeekDates(weekOf)
+
+    return (
+        <div className='container'>
+            <div className='controls'>
+                <span onClick={() => onClick(previousWeek)} id="previous">&#12296;</span>
+            </div>
+            <div className='week'>
+                <h3>{formatWeek(weekOf)}</h3>
+            </div>
+            <div className='controls'>
+                <span onClick={() => onClick(nextWeek)} id="next">&#12297;</span>
+            </div>
+        </div>
+    )
+}
+
+export default WeekSpinner

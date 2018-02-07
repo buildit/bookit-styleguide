7b0a2618a2c3b56b9f71db879a1df0b6b04977eb
Victor Okon <victor.s.okon@gmail.com>
2018-02-06 22:22:42 -0500

    updates to header to fix flex direction

--- catalog/react/header/myBookingHeader.jsx
+++ catalog/react/header/myBookingHeader.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 
 const MyBookingHeader = ({}) => (
-    <div className="mybookingHeader">
-        <div className="header_wrapping">
-          <h2>My Bookings</h2>
-            <img src='../../../images/close.svg' alt="Close booking form and go back home" />
-        </div>
+    <div className="myBookingHeader">
+        <h2>My Bookings</h2>
+        <img src='../../../images/close.svg' alt="Close booking form and go back home" />
     </div>    
     )
     
